8a6f783a0085c65e93b595d6f95924590168bcd5
顾翔 <c0710204@gmail.com>
2014-09-20 09:53:12 +0800

    add cmd for core
    Signed-off-by: 顾翔 <c0710204@gmail.com>

--- js/metro-core.js
+++ js/metro-core.js
@@ -1,39 +1,53 @@
-
-(function($){
-    $.Metro = function(params){
-        params = $.extend({
-        }, params);
-    };
-
-    $.Metro.getDeviceSize = function(){
-        var device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        var device_height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-        return {
-            width: device_width,
-            height: device_height
+(function (factory) {
+        if (typeof define === 'function' && define.amd) {
+            // AMD. Register as an anonymous module.
+            define(['jquery', 'jquery.mousewhell', 'jquery.widget'], factory);
+        } else if (typeof exports === 'object') {
+            // Node/CommonJS style for Browserify
+            module.exports = factory;
+        } else {
+            // Browser globals
+            factory(jQuery);
         }
     }
 
-})(jQuery);
+    (function ($) {
+        (function ($) {
+            $.Metro = function (params) {
+                params = $.extend({}, params);
+            };
 
-$(function(){
-    $('html').on('click', function(e){
-        $('.dropdown-menu').each(function(i, el){
-            if (!$(el).hasClass('keep-open') && $(el).css('display')=='block') {
-                $(el).hide();
+            $.Metro.getDeviceSize = function () {
+                var device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+                var device_height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+                return {
+                    width: device_width,
+                    height: device_height
+                }
             }
-        });
-    });
-});
 
-$(function(){
-    $(window).on('resize', function(){
-        if (METRO_DIALOG) {
-            var top = ($(window).height() - METRO_DIALOG.outerHeight()) / 2;
-            var left = ($(window).width() - METRO_DIALOG.outerWidth()) / 2;
-            METRO_DIALOG.css({
-                top: top, left: left
+        })(jQuery);
+
+        $(function () {
+            $('html').on('click', function (e) {
+                $('.dropdown-menu').each(function (i, el) {
+                    if (!$(el).hasClass('keep-open') && $(el).css('display') == 'block') {
+                        $(el).hide();
+                    }
+                });
             });
-        }
-    });
-});
+        });
+
+        $(function () {
+            $(window).on('resize', function () {
+                if (METRO_DIALOG) {
+                    var top = ($(window).height() - METRO_DIALOG.outerHeight()) / 2;
+                    var left = ($(window).width() - METRO_DIALOG.outerWidth()) / 2;
+                    METRO_DIALOG.css({
+                        top: top,
+                        left: left
+                    });
+                }
+            });
+        });
+    }));
